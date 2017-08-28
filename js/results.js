4ca1027dafd9bb47128deb434d4df255888f61cd
Playingtygre <kymiehsu123@gmail.com>
2017-08-28 14:24:04 -0700

    tiger-buildObject,made for loop to build object

--- js/results.js
+++ js/results.js
@@ -1,19 +1,24 @@
 'use strict';
-//create a constructor function//
+//create a object constructor creating the buddies//
 
-function Question (first, last) {
-  this.name = Boolean;
-  this.owl = Boolean;
-  this.introvert = Boolean;
+function Buddy (name,birdOwl,personality) {
+  this.name = name;
+  this.birdOwl = birdOwl;
+  this.personality = personality;
   this.tally = 0;
+
+  function addBuddy(){
+    buddyArray.push(this);
+  }
+  addBuddy();
+};
+
+var infoArray = [['Kinley','owl','intro']];
+var buddyArray = [];
+
+for (var i = 0; i < infoArray.length; i++) {
+  var buddyName = infoArray[i][0];
+  var buddyBirdOwl = infoArray[i][1];
+  var buddyPersonality = infoArray[i][2];
+  var buddy = new Buddy(buddyName,buddyBirdOwl,buddyPersonality);
 }
-
-
-var Answer1 = new Question ("true", "false");
-var Answer2 = new Question ("ture", "false");
-
-var AnswerArray = []Question;
-
-question array
-
-method
