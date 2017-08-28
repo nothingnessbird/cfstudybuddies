9b06fb210af59f84df7e7e05417b91e57e15a760
Kinley Ramson <kinleyramson@gmail.com>
2017-08-28 15:11:20 -0700

    working on gettig info from form

--- js/form.js
+++ js/form.js
@@ -1,3 +1,13 @@
 'use strict';
 
-var form = document.getElementById('id');
+var userArray = [];
+var form = document.getElementById('form');
+form.addEventListener('submit',formHarvester);
+
+function formHarvester(event) {
+  event.preventDefault();
+  var name = this.elements.name.value;
+  var owlBird = this.elements.owlBird.value;
+  var personality = this.elements.personality.value;
+  userArray.push(name,owlBird,personality);
+};
