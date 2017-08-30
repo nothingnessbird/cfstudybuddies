59644cc9294cbadf33a8633c9fb4545d2cb4c306
stariel <ariel.altaras@gmail.com>
2017-08-30 11:04:38 -0700

    tiger-addedscore

--- js/score.js
+++ js/score.js
@@ -3,10 +3,18 @@
 function scoreMaker () {
   user = new Buddy(user[0], user[1], user[2]);
   for (var i = 0; i < buddyArray.length; i++) {
-    if (user.birdOwl === buddyArray[i].birdOwl)
+    if (user.birdOwl === buddyArray[i].birdOwl) {
       buddyArray[i].tally++;
-    if (user.personality = buddyArray[i].personality)
+    }
+    if (user.personality = buddyArray[i].personality) {
       buddyArray[i].tally++;
+    }
+    if (user.noise === 'quiet') {
+      if (buddyArray[i].noise === 'quiet') {
+        buddyArray[i].tally += 2;
+      }
+    }
+    
   };
 };
 scoreMaker();
