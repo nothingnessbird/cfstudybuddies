1ddaf63351950301056a678b7152cba877bb3e77
stariel <ariel.altaras@gmail.com>
2017-08-30 12:16:12 -0700

    kinley drove adding the catdog conditionals to the scoremaker function while everyone else mob navigated

--- js/score.js
+++ js/score.js
@@ -22,20 +22,49 @@ function scoreMaker () {
         buddyArray[i].tally ++;
       }
     }
-    if (user.noise === 'talk'){
+    if (user.noise === 'talk') {
       if (buddyArray[i].noise === 'talk'){
         buddyArray[i].tally += 2;
       }
       else if (buddyArray[i].noise === 'music'){
-        buddyArray[i].tally ++;
+        buddyArray[i].tally++;
       }
     }
-    if (user.noise === 'busy'){
+    if (user.noise === 'busy') {
       if (buddyArray[i].noise === 'busy'){
         buddyArray[i].tally += 2;
       }
       else if (buddyArray[i].noise === 'talk' || buddyArray[i].noise === 'music') {
-        buddyArray[i].tally ++;
+        buddyArray[i].tally++;
+      }
+    }
+    if (user.catDog === 'cat') {
+      if (buddyArray[i].name === 'Java') {
+        buddyArray[i].tally += 5;
+      }
+      else if (buddyArray[i].catDog === 'cat' || buddyArray[i].catDog === 'both') {
+        buddyArray[i].tally++;
+      }
+    }
+    if (user.catDog === 'dog') {
+      if (buddyArray[i].name === 'Asha') {
+        buddyArray[i].tally += 5;
+      }
+      else if (buddyArray[i].catDog === 'dog' || buddyArray[i].catDog === 'both') {
+        buddyArray[i].tally++;
+      }
+    }
+    if (user.catDog === 'both') {
+      if (buddyArray[i].name === 'Java' || buddyArray[i].name === 'Asha') {
+        buddyArray[i].tally += 5;
+      }
+      else if (!buddyArray[i].catDog === 'neither') {
+        buddyArray[i].tally++;
+      }
+    }
+    if (user.catDog === 'neither') {
+      if (buddyArray[i].name === 'Java' || buddyArray[i].name === 'Asha') {
+        buddyArray.tally -= 5;
       }
     }
   };
